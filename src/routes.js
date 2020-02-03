8365aafdfe5d5db041680becf9bd67fb0cc11481
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-02-03 21:28:14 -0200

    feat(*): Added configuration for linting and sequelize

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'Success!' });
-})
+  return res.json({ message: 'Success!' });
+});
 
 export default routes;
