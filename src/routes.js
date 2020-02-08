fd24a70af289d1867ac0542a09ff3c44efd97bdf
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-02-08 16:21:50 -0200

    feat(authorization): Created auth middleware

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,16 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SesseionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SesseionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
 
 export default routes;
