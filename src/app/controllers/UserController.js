1a2fdf6a67f568ac291bfe657845178618e83121
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-02-08 16:22:05 -0200

    feat(update): Created update route

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,6 +17,34 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldpassword } = req.body;
+    const { userId } = req;
+
+    const user = await User.findByPk(userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        res.status(400).json({ error: 'E-mail already registered!' });
+      }
+    }
+
+    if (oldpassword && !(await user.checkPassword(oldpassword))) {
+      return res.status(401).json({ error: 'Password does not match!' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
