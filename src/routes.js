07d7bcb0d2aa52d97bef6077ec8a94a9c7d8f231
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-02-16 20:41:02 -0300

    feat(files): Created files controller

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SesseionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SesseionController.store);
@@ -13,5 +19,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.post('/file', upload.single('file'), FileController.store);
 
 export default routes;
