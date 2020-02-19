92a4eb339e44686abecf2d351457400cdcbc22f3
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-02-19 18:42:07 -0300

    feat(schedule): Created schedule controller

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SesseionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -27,5 +28,7 @@ routes.post('/file', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
