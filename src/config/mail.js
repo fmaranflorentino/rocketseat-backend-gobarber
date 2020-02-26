38495ccd69ed453ec538acc17069d7d6a3f6bdf4
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-02-26 11:11:20 -0300

    feat(mail): Created mail configuration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '08493398cebfae',
+    pass: '0112c1369075e6',
+  },
+  default: {
+    from: 'Go baraber - FMF',
+  },
+};
