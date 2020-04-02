e258bbe29b5e258429d3f50489004ac16a04d4a3
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-04-02 13:51:46 -0300

    feat(AvailableController): create available controller and index method

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SesseionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AvailableController from './app/controllers/AvailableController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
@@ -24,6 +25,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/file', upload.single('file'), FileController.store);
 
